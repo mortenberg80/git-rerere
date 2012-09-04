bbea0faa498907e26925e06859eef385543cd4bd
Morten Berg <mortenberg80@gmail.com>
2012-09-04 22:22:07 +0200

    Initial commit

---
+++ src/main/java/no/iterate/App.java
@@ -0,0 +1,15 @@
+package no.iterate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+
+    public void sayHello(String name) {
+        System.out.println("Hello, " + name);
+    }
+}
